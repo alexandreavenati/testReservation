d246b3dc700b81a685e6158f3bedc64a259e5d6f
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-04-15 10:23:03 +0200

    Projet Réservation avec méthode pay et leaveComment

--- model/reservation-model.php
+++ model/reservation-model.php
@@ -21,7 +21,16 @@ class Reservation
 
     public $cleaningOption;
 
-    // ajout de paramètres sur les valeurs qui ont tendances à changer selon les réservations
+    public $cancelledAt;
+
+    public $paidAt;
+
+    public $commentedAt;
+
+    public $comment;
+
+    // ajout de paramètres sur les valeurs qui ont tendances à changer selon les réservations 
+    // (méthode pour réaliser une réservation)
     public function __construct($name, $place, $startDate, $endDate, $cleaningOption)
     {
         // valeurs envoyées par l'utilisateur
@@ -46,11 +55,32 @@ class Reservation
         $this->status = "CART";
     }
 
-    // création d'une fonction "cancel"
+    // création d'une méthode pour annuler la réservation
     public function cancel() {
         // si le statut de la réservation est "CART" alors on peut passer le statut en "CANCELLED"
         if ($this->status === "CART") {
             $this->status = "CANCELLED";
+            $this->cancelledAt = new DateTime();
+        }
+    }
+
+    // création d'une méthode pour payer la réservation
+    public function pay() {
+        // si le statut de la réservation est "CART" alors on peut passer le statut en "PAID"
+        if ($this->status === "CART") {
+            $this->status = "PAID";
+            $this->paidAt = new DateTime();
+        }
+    }
+
+    // création d'une méthode pour laisser un commentaire
+    public function leaveComment($comment) {
+        // si le statut de la réservation est "PAID" alors on peut passer le statut en "COMMENTED" en ajoutant un commentaire 
+        // en paramètre
+        if ($this->status === "PAID") {
+            $this->status = "COMMENTED";
+            $this->commentedAt = new DateTime();
+            $this->comment = $comment;
         }
     }
 }
@@ -65,8 +95,11 @@ $cleaningOption = false;
 // création de la réservation (paramètres nécessaires car la fonction "__construct" a des paramètres pour fonctionner)
 $reservation = new Reservation($name, $place, $startDate, $endDate, $cleaningOption);
 
-// Appel de la fonction "cancel" sur l'objet "reservation" pour révoquer la demande de réservation
-$reservation->cancel();
+// Appel de la fonction "pay" sur l'objet "reservation" pour payer la réservation
+$reservation->pay();
+
+// Appel de la fonction "leaveComment" sur l'objet "reservation" pour commenter après avoir payé la réservation
+$reservation->leaveComment("Bonne chambre, propre et confortable.");
 
 var_dump($reservation);
 die;
