448e4cda464511ba8c955cd21eac29c712e865a3
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-04-16 10:41:30 +0200

    Projet Réservation avec page d'annulation de la réservation et changement de statut

--- controller/cancel-reservation-controller.php
+++ controller/cancel-reservation-controller.php
@@ -10,8 +10,9 @@ $reservationForUser = findReservationForUser();
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     try {
 
-        // permet de vérifier l'existence de l'objet "reservationForUser" et si cet objet est une instance d'une 
-        // classe précise (ici instanceof vérifie si "reservationForUser" est un objet et si "reservationForUser" est une instance
+        // permet de vérifier si la variable "reservationForUser" n'est pas nulle et vérifie l'existence de l'objet
+        // "reservationForUser" et si cet objet est une instance d'une  classe précise
+        // (ici instanceof vérifie si "reservationForUser" est un objet et si "reservationForUser" est une instance 
         // de la classe "Reservation")
         if ($reservationForUser && $reservationForUser instanceof Reservation) {
 
