5139f6312a4be35609017b33b6c94fb88fe32093
Ronnawit Raksachob <alexandre.avenati@lapiscine.pro>
2025-04-16 10:35:15 +0200

    Projet Réservation avec page d'annulation de la réservation et changement de statut

--- view/cancel-reservation-view.php
+++ view/cancel-reservation-view.php
@@ -4,22 +4,6 @@
 
     <h2>Annuler une réservation</h2>
 
-    <!-- Création d'un formulaire pour envoyer ses données -->
-    <form method="POST">
-
-        <div>
-            <button type="submit">Annuler la réservation</button>
-        </div>
-
-    </form>
-
-    <?php if (isset($errorMessage)) { ?>
-    <!-- Message d'erreur si il y en a -->
-    <div>
-        <h3><?php echo $errorMessage; ?></h3>
-    </div>
-    <?php } ?>
-
     <!-- Résumé de la réservation si il y en a une et permet de vérifier si une variable est un objet et si cet objet est une instance d'une classe précise 
         (ici instanceof vérifie si "reservationForUser" est un objet et si "reservationForUser" est une instance
         de la classe "Reservation") -->
@@ -32,6 +16,22 @@
             <p><strong>Option de ménage : <?php echo $reservationForUser->cleaningOption ? "oui" : "non"; ?></strong></p>
             <p><strong>Prix total : <?php echo $reservationForUser->totalPrice ?></strong></p>
             <p><strong>Statut : "<?php echo $reservationForUser->status ?>"</strong></p>
+        </div>
+    <?php } ?>
+
+    <!-- Création d'un formulaire pour annuler la réservation-->
+    <form method="POST">
+
+        <div>
+            <button type="submit">Annuler la réservation</button>
+        </div>
+
+    </form>
+
+    <?php if (isset($errorMessage)) { ?>
+        <!-- Message d'erreur si il y en a -->
+        <div>
+            <h3><?php echo $errorMessage; ?></h3>
         </div>
     <?php } ?>
 
